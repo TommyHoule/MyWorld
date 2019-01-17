76575aaade957bcff7660e4f589c79c357934e58
tommyhoule <tommyhoule@outlook.fr>
2019-01-17 12:58:15 -0500

    Suppression des doublons d anciens fichier maintenant transféré dans /src/

--- MyWorld/Exceptions/AlreadyExistingWorldException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-public class AlreadyExistingWorldException : Exception
-{
-    public AlreadyExistingWorldException()
-        : base("A world with the same name already exists in this universe !")
-    {
-    }
-}
